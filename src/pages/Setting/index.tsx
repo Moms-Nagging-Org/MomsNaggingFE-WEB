7aa666dc559a9738348c0263e9d156fece498ddc
tooooo1 <wer4272@nate.com>
2022-04-28 19:00:56 +0900

    feat : setting page 추가

---
+++ src/pages/Setting/index.tsx
@@ -0,0 +1,12 @@
+import SideBar from '../../components/SideBar';
+import * as Styled from './styled';
+
+const Setting = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default Setting;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Login } from './Login';
 export { default as Temp } from './Temp';
+export { default as Setting } from './Setting';
