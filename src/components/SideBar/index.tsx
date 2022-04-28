2aac9fb5217bff9687f71be0d039acadc480f152
tooooo1 <wer4272@nate.com>
2022-04-28 20:10:33 +0900

    feat : SideBar 추가 기능 구현
    클릭시 이벤트 구현 및 로그아웃 구현

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,36 +1,53 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as Styled from './styled';
 import {
-  User,
-  DashBoard,
-  Habit,
-  Logo,
-  SideBarLogo,
-  Push,
-  Question,
-  Withdrawal,
+  UserSVG,
+  DashBoardSVG,
+  HabitSVG,
+  SideBarLogoSVG,
+  PushSVG,
+  QuestionSVG,
+  WithdrawalSVG,
+  LogoutSVG,
 } from '../../assets';
 
 const SideBar = () => {
+  let navigate = useNavigate();
+  const [checking, setChecking] = useState<string>('대시보드');
   const menuList = [
-    { name: '대시보드', option: <DashBoard /> },
-    { name: '회원관리', option: <User /> },
-    { name: 'PUSH알림', option: <Push /> },
-    { name: '추천습관', option: <Habit /> },
-    { name: '문의사항', option: <Question /> },
-    { name: '탈퇴관리', option: <Withdrawal /> },
+    { name: '대시보드', option: <DashBoardSVG /> },
+    { name: '회원관리', option: <UserSVG /> },
+    { name: 'PUSH알림', option: <PushSVG /> },
+    { name: '추천습관', option: <HabitSVG /> },
+    { name: '문의사항', option: <QuestionSVG /> },
+    { name: '탈퇴관리', option: <WithdrawalSVG /> },
   ];
+
+  const clickFunc = (e: any) => {
+    setChecking(e.target.outerText);
+  };
+
   return (
     <Styled.Wrapper>
-      <SideBarLogo />
+      <SideBarLogoSVG />
       <Styled.Side>
         {menuList.map(v => {
           return (
-            <Styled.SideDetail key={v.name}>
+            <Styled.SideDetail
+              key={v.name}
+              id={v.name}
+              checking={checking}
+              onClick={e => clickFunc(e)}
+            >
               {v.option} <span style={{ marginLeft: '1rem' }}>{v.name}</span>
             </Styled.SideDetail>
           );
         })}
       </Styled.Side>
+      <Styled.SideDetail id="logout" onClick={() => navigate('/')}>
+        <LogoutSVG /> <span style={{ marginLeft: '1rem' }}>로그아웃</span>
+      </Styled.SideDetail>
     </Styled.Wrapper>
   );
 };
--- src/components/SideBar/styled.ts
+++ src/components/SideBar/styled.ts
@@ -7,20 +7,33 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 3rem 0;
+  padding-top: 3rem;
 `;
 
 export const Side = styled.div`
-  margin: 4rem 0;
+  margin-top: 4rem;
   width: 70%;
+  height: 78vh;
 `;
 
-export const SideDetail = styled.div`
+export const SideDetail = styled.div<{ checking?: string }>`
   color: white;
   font-size: 1.2rem;
-  margin-bottom: 3vh;
+  margin-bottom: 1vh;
   display: flex;
   align-items: center;
   cursor: pointer;
   border-radius: 10px;
+  padding: 0.5rem;
+
+  &#logout {
+    width: 70%;
+    padding-bottom: 5vh;
+  }
+
+  &#${({ checking }) => checking} {
+    font-weight: bold;
+    background-color: white;
+    color: #f43e51;
+  }
 `;
