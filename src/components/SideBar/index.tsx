bf3c5cbc09f14203c46199d19adee8330b3275d6
tooooo1 <wer4272@nate.com>
2022-04-28 17:22:11 +0900

    feat : sidebar component 추가

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,12 @@
+import * as Styled from './styled';
+import SideBarLogo from '../../assets/SideBarLogo';
+
+const SideBar = () => {
+  return (
+    <Styled.Wrapper>
+      <SideBarLogo />
+    </Styled.Wrapper>
+  );
+};
+
+export default SideBar;
---
+++ src/components/SideBar/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #f43e51;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
