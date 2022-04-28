0f291ad8297e6ca617d2b14b39035eff3492ee7b
tooooo1 <wer4272@nate.com>
2022-04-28 17:22:22 +0900

    feat : setting 임시 화면

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,5 +1,19 @@
+import SideBar from '../../components/SideBar';
+import * as Styled from './styled';
+import { Positioner } from '../../components/Wrapper/styled';
+import Logo from '../../assets/Logo';
+
 const Setting = () => {
-  return <div></div>;
+  return (
+    <Positioner>
+      <Styled.GlobalStyle />
+      <Styled.Wrapper>
+        <Logo />
+        <Styled.Temp>현재 퉁이리가</Styled.Temp>
+        <Styled.Temp id="bottom">열심히 개발중이에요!</Styled.Temp>
+      </Styled.Wrapper>
+    </Positioner>
+  );
 };
 
 export default Setting;
--- src/pages/Setting/styled.ts
+++ src/pages/Setting/styled.ts
@@ -1,1 +1,25 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #F43E51;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Temp = styled.div`
+  font-family: 'Pretendard-Medium';
+  font-size: 2rem;
+  margin: 1.5rem 0;
+  color: white;
+  opacity: 0.7;
+
+  &#bottom {
+    margin: 0;
+  }
+`;
