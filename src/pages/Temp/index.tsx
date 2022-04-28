6cd749f0348a3e4d77b7032a93d90f004445c774
tooooo1 <wer4272@nate.com>
2022-04-28 20:10:05 +0900

    feat : 임시화면 갱신
    sideBar 추가

--- src/pages/Temp/index.tsx
+++ src/pages/Temp/index.tsx
@@ -1,18 +1,17 @@
 import SideBar from '../../components/SideBar';
 import * as Styled from './styled';
 import { Positioner } from '../../components/Wrapper/styled';
-import Logo from '../../assets/Logo';
+import LogoSVG from '../../assets/LogoSVG';
 
 const Temp = () => {
   return (
-    <Positioner>
-      <Styled.GlobalStyle />
+    <Styled.Wrapper>
+      <SideBar />
       <Styled.Wrapper>
-        <Logo />
-        <Styled.Temp>현재 퉁이리가</Styled.Temp>
-        <Styled.Temp id="bottom">열심히 개발중이에요!</Styled.Temp>
+        <LogoSVG />
+        <Styled.Temp>현재 퉁이리가 열심히 개발 중이에요!</Styled.Temp>
       </Styled.Wrapper>
-    </Positioner>
+    </Styled.Wrapper>
   );
 };
 
--- src/pages/Temp/styled.ts
+++ src/pages/Temp/styled.ts
@@ -1,22 +1,15 @@
-import styled, { createGlobalStyle } from 'styled-components';
-
-export const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #F43E51;
-  }
-`;
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
 `;
 
 export const Temp = styled.div`
   font-family: 'Pretendard-Medium';
   margin: 1.5rem 0;
-  color: white;
-  opacity: 0.7;
+  color: black;
+  font-weight: bold;
 
   &#bottom {
     margin: 0;
