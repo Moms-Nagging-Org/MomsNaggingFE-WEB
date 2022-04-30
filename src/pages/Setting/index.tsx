ddf2980335356218d38153e4ea8ed78db35cd0e0
tooooo1 <wer4272@nate.com>
2022-04-30 20:59:01 +0900

    update : component 추가

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -8,7 +8,10 @@ const Setting = () => {
       <SideBar />
       <div>
         <Styled.SelectTitle>대시보드</Styled.SelectTitle>
-        <DashBoard />
+        <DashBoard optionTitle="1.서비스" />
+        <DashBoard optionTitle="2.IOS" />
+        <DashBoard optionTitle="3.AOS" />
+        <DashBoard optionTitle="4.상장" />
       </div>
     </Styled.Wrapper>
   );
