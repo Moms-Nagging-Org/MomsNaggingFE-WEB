450693a1f1d3565af5b47a5e792af4be6095b1c6
tooooo1 <wer4272@nate.com>
2022-04-30 20:59:19 +0900

    update : 배포 갱신

--- src/pages/Temp/index.tsx
+++ src/pages/Temp/index.tsx
@@ -1,16 +1,22 @@
 import SideBar from '../../components/SideBar';
 import * as Styled from './styled';
-import { Positioner } from '../../components/Wrapper/styled';
+import DashBoard from '../DashBoard';
 import LogoSVG from '../../assets/LogoSVG';
 
 const Temp = () => {
   return (
     <Styled.Wrapper>
       <SideBar />
-      <Styled.Wrapper>
+      <div>
+        <Styled.SelectTitle>대시보드</Styled.SelectTitle>
+        <DashBoard optionTitle="1.서비스" />
+        <DashBoard optionTitle="2.IOS" />
+        <DashBoard optionTitle="3.AOS" />
+        <DashBoard optionTitle="4.상장" />
+
         <LogoSVG />
         <Styled.Temp>현재 퉁이리가 열심히 개발 중이에요!</Styled.Temp>
-      </Styled.Wrapper>
+      </div>
     </Styled.Wrapper>
   );
 };
--- src/pages/Temp/styled.ts
+++ src/pages/Temp/styled.ts
@@ -15,3 +15,12 @@ export const Temp = styled.div`
     margin: 0;
   }
 `;
+
+export const SelectTitle = styled.div`
+  display: flex;
+  margin: 4rem;
+  flex-direction: column;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #f43e51;
+`;
