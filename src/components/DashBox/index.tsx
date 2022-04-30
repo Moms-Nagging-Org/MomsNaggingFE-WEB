eb69f6749723af55f778cbcb95c6462231f745ba
tooooo1 <wer4272@nate.com>
2022-04-30 19:59:22 +0900

    feat : DashBox component 생성

---
+++ src/components/DashBox/index.tsx
@@ -0,0 +1,12 @@
+import * as Styled from './styled';
+
+const DashBox = () => {
+  return (
+    <Styled.Wrapper>
+      <Styled.Title>서비스 운영</Styled.Title>
+      <Styled.Content>121일째</Styled.Content>
+    </Styled.Wrapper>
+  );
+};
+
+export default DashBox;
---
+++ src/components/DashBox/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 10vw;
+  height: 10vh;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+  border-radius: 20px;
+  margin: 0 2vw;
+`;
+
+export const Title = styled.div`
+  font-weight: bold;
+`;
+
+export const Content = styled.div`
+  text-decoration: underline;
+`;
