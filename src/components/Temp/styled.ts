3122ce6de7e5287a7ec83e760d128c2f932144e7
tooooo1 <wer4272@nate.com>
2022-05-02 21:06:10 +0900

    feat : page -> component 변경
    Temp page 에서 Temp component로 변경 및 manage Util 추가

--- src/components/Temp/styled.ts
+++ src/components/Temp/styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: column;
 `;
 
 export const Temp = styled.div`
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as Login } from './Login';
-export { default as Temp } from './Temp';
 export { default as Setting } from './Setting';
 export { default as DashBoard } from './DashBoard';
