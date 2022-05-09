a5219dba48989047d8459aef68ba7e2ae8c57027
tooooo1 <wer4272@nate.com>
2022-05-09 18:37:35 +0900

    feat : 습관 테이블 추가

--- src/pages/HabitManage/index.tsx
+++ src/pages/HabitManage/index.tsx
@@ -1,15 +1,20 @@
 import * as Styled from './styled';
-import LogoSVG from '../../assets/LogoSVG';
 import TabBar from '../../components/TabBar';
 
 const HabitManage = () => {
   return (
     <Styled.Wrapper>
       <TabBar />
-      <div>
-        <LogoSVG />
-        <Styled.Temp>현재 퉁이리가 열심히 개발 중이에요!</Styled.Temp>
-      </div>
+      <Styled.Table>
+        <Styled.THead>
+          <Styled.THeadRow>
+            <Styled.HabitName>추천 습관 이름</Styled.HabitName>
+            <Styled.Friendly>(1) 다정한 엄마</Styled.Friendly>
+            <Styled.Cool>(2) 냉정한 엄마</Styled.Cool>
+            <Styled.Angry>(3) 화가 많은 엄마</Styled.Angry>
+          </Styled.THeadRow>
+        </Styled.THead>
+      </Styled.Table>
     </Styled.Wrapper>
   );
 };
--- src/pages/HabitManage/styled.ts
+++ src/pages/HabitManage/styled.ts
@@ -25,3 +25,37 @@ export const SelectTitle = styled.div`
   font-weight: bold;
   color: #f43e51;
 `;
+
+export const Table = styled.table`
+  width: 100%;
+  text-align: center;
+  font-size: 0.6rem;
+  margin-top: 1rem;
+`;
+
+export const THead = styled.thead`
+  background-color: #f6f6f6;
+`;
+
+export const THeadRow = styled.tr``;
+
+export const TBody = styled.tbody`
+  font-family: 'Pretendard-SemiBold';
+`;
+
+export const HabitName = styled.td`
+  width: 25%;
+  padding: 1rem 0;
+`;
+export const Friendly = styled.td`
+  width: 25%;
+  padding: 1rem 0;
+`;
+export const Cool = styled.td`
+  width: 25%;
+  padding: 1rem 0;
+`;
+export const Angry = styled.td`
+  width: 25%;
+  padding: 1rem 0;
+`;
