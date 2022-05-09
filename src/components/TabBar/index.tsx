c4d82d16f8e5a5d3a59f9ef04c6dbef0a89a13f9
tooooo1 <wer4272@nate.com>
2022-05-09 18:39:21 +0900

    update : 메뉴 클릭 활성화

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -1,22 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as Styled from './styled';
 import { AddSVG } from '../../assets';
+import { useRecoilValue } from 'recoil';
+import { selectTitleState } from '../../recoil/recoil';
+
+const menu = [
+  [
+    { name: '정기성', option: 'many' },
+    { name: '일회성', option: 'one' },
+  ],
+  [
+    { name: '생활', option: 'life' },
+    { name: '운동', option: 'exercise' },
+    { name: '성장', option: 'growth' },
+    { name: '생산성', option: 'productivity' },
+    { name: '식습관', option: 'eating' },
+    { name: '기타', option: 'andSoOn' },
+  ],
+];
 
 const TabBar = () => {
-  const [menuCheck, setMenuCheck] = useState(0);
-  const [check, setCheck] = useState('many');
+  const selectTitle = useRecoilValue<string>(selectTitleState); // 사이드 메뉴(PUSH, 추천습관)
+  const [menuChoice, setMenuChoice] = useState({ number: 0, option: 'many' }); //사이드 메뉴(PUSH, 추천습관) 초기 활성화
+  const [menuCheck, setMenuCheck] = useState(0); //세부 메뉴
+  const [check, setCheck] = useState(menu[menuChoice.number][menuCheck].option); //체크 표시
   const clickFunc = (e: any, index: number) => {
     setMenuCheck(index);
     setCheck(e.target.id);
+    console.log(check);
   };
-  const menu = [
-    { name: '정기성', option: 'many' },
-    { name: '일회성', option: 'one' },
-  ];
+
+  useEffect(() => {
+    if (selectTitle === 'PUSH알림') {
+      setMenuChoice({ ...menuChoice, number: 0, option: 'many' });
+      console.log(check);
+    } else if (selectTitle === '추천습관') {
+      setMenuChoice({ ...menuChoice, number: 1, option: 'life' });
+      console.log(check);
+    }
+  }, [selectTitle]);
+
   return (
     <Styled.Wrapper id="top">
       <Styled.Wrapper>
-        {menu.map((v, index) => (
+        {menu[menuChoice.number].map((v, index) => (
           <Styled.MenuTitle
             key={v.name}
             id={v.option}
