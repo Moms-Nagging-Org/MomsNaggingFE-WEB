9cb1a3f77c157933efcb7dce782030aa90db25a4
tooooo1 <wer4272@nate.com>
2022-05-09 18:37:26 +0900

    feat : PUSH 테이블 추가

--- src/pages/PushManage/index.tsx
+++ src/pages/PushManage/index.tsx
@@ -1,15 +1,21 @@
 import * as Styled from './styled';
-import LogoSVG from '../../assets/LogoSVG';
 import TabBar from '../../components/TabBar';
 
 const PushManage = () => {
   return (
     <Styled.Wrapper>
       <TabBar />
-      <div>
-        <LogoSVG />
-        <Styled.Temp>현재 퉁이리가 열심히 개발 중이에요!</Styled.Temp>
-      </div>
+      <Styled.Table>
+        <Styled.THead>
+          <Styled.THeadRow>
+            <Styled.Day>요일</Styled.Day>
+            <Styled.Time>시간</Styled.Time>
+            <Styled.Friendly>(1) 다정한 엄마</Styled.Friendly>
+            <Styled.Cool>(2) 냉정한 엄마</Styled.Cool>
+            <Styled.Angry>(3) 화가 많은 엄마</Styled.Angry>
+          </Styled.THeadRow>
+        </Styled.THead>
+      </Styled.Table>
     </Styled.Wrapper>
   );
 };
