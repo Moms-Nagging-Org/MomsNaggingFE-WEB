939d120150063f1c73a4c3c19b0a6dcaa2429607
tooooo1 <wer4272@nate.com>
2022-08-16 04:17:13 +0900

    fix: router 변경

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,30 +1,45 @@
-import { useState, useCallback } from 'react';
-import * as Styled from './styled';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../../assets/LogoSVG';
 import { Positioner } from '../../components/Wrapper/styled';
+import * as Styled from './styled';
 
 const Login = () => {
   let navigate = useNavigate();
-  const [id, setId] = useState<string>('');
-  const [pw, setPw] = useState<string>('');
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
-  const onChangeID = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setId(event.target.value);
-  }, []);
-  const onChangePW = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setPw(event.target.value);
-  }, []);
+  const onChangeID = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setId(event.target.value);
+    },
+    []
+  );
+  const onChangePW = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPw(event.target.value);
+    },
+    []
+  );
 
   return (
     <Positioner>
       <Styled.GlobalStyle />
       <Styled.Wrapper>
         <Logo />
-        <Styled.Input onChange={onChangeID} id="top" type="text" placeholder="아이디" />
-        <Styled.Input onChange={onChangePW} type="password" placeholder="비밀번호" />
+        <Styled.Input
+          onChange={onChangeID}
+          id="top"
+          type="text"
+          placeholder="아이디"
+        />
+        <Styled.Input
+          onChange={onChangePW}
+          type="password"
+          placeholder="비밀번호"
+        />
         <Styled.Button
-          onClick={() => navigate('/setting')}
+          onClick={() => navigate('/dashboard')}
           background="#f28590"
           disabled={!(id === 'moms' && pw === 'nagging')}
         >
