7e6ba2232c26de8cb720ab63b844b9c132b49d4f
tooooo1 <wer4272@nate.com>
2022-08-16 05:13:40 +0900

    style: antd custom

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,79 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Noto Sans KR', sans-serif;
   }
+
+  .ant-menu-item-selected {
+    background-color: rgba(255, 255, 255, 0.2) !important;
+  }
+
+  .ant-menu-inline .ant-menu-item::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    content: '';
+  }
+
+  .ant-layout-header {
+    background: #f0f2f5;
+  }
+
+  .ant-layout-sider {
+    background: #f43e51;
+  }
+  .ant-layout-sider-children{
+    height: 100vh;
+  }
+
+  .ant-menu.ant-menu-dark {
+    color: rgba(255, 255, 255, 0.65);
+    background: #f43e51;
+  }
+
+  .ant-layout-content {
+    padding: 25px 50px;
+  }
+
+  .ant-pagination-prev .ant-pagination-item-link, .ant-pagination-next .ant-pagination-item-link {
+    border: none;
+    background-color: none;
+  }
+
+  .ant-pagination-item-active {
+    border-color: #f43e51;
+
+    a {
+      color: #f43e51;
+    }
+  }
+
+  .ant-btn-primary {
+    background: none;
+    border: none;
+    box-shadow: none;
+  }
+
+  .ant-input-search > .ant-input-group > .ant-input-group-addon:last-child .ant-input-search-button {
+    padding-bottom: 0;
+  }
+
+  .ant-input-search .ant-input-group .ant-input-affix-wrapper:not(:last-child) {
+    border: none;
+    border-radius: 5px;
+    padding: 0.5rem;
+  }
+
+  .ant-btn-primary:hover, .ant-btn-primary:focus {
+    color: none;
+    border-color: none;
+    background: none;
+  }
+
+  .ant-btn-primary:active {
+    color: none;
+    border-color: none;
+    background: none;
+  }
 `;
 
 const App = () => {
