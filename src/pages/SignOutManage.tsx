f7851bb3c4fb4def4e69ebc8eca0f5dd80adf51e
tooooo1 <wer4272@nate.com>
2022-09-04 01:35:00 +0900

    feat: 탈퇴페이지 api 연동

--- src/pages/SignOutManage.tsx
+++ src/pages/SignOutManage.tsx
@@ -4,7 +4,7 @@ import ManageUtil from '@/components/ManageUtil';
 import useFetch from '@/hooks/useFetch';
 import { Table } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import {
   createSearchParams,
   useNavigate,
@@ -12,14 +12,17 @@ import {
 } from 'react-router-dom';
 
 interface SignOutResponse {
-  result: SignOut[];
-  total: number;
+  content: SignOut[];
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  empty: boolean;
 }
 
 interface SignOut {
+  context: string;
   id: number;
   title: string;
-  context: string;
   createdAt: string;
 }
 
@@ -40,34 +43,17 @@ const columns: ColumnsType<SignOut> = [
     dataIndex: 'title',
     key: 'title',
   },
-  // {
-  //   title: '문의 내용',
-  //   key: 'context',
-  //   dataIndex: 'context',
-  // },
 ];
 
 const SignOutManage = () => {
-  const {
-    data: questions,
-    loading,
-    error,
-  } = useFetch<SignOutResponse>('/sign-out', 'GET');
+  const { data: questions, loading } = useFetch<SignOutResponse>(
+    '/sign-out',
+    'GET'
+  );
 
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [page, setPage] = useState(parseInt(searchParams.get('page') ?? '1'));
-  let newQuestions = {
-    result: questions ?? undefined,
-    total: 4,
-  };
-
-  useEffect(() => {
-    newQuestions = {
-      result: questions ?? undefined,
-      total: 4,
-    };
-  }, [questions]);
 
   return (
     <CustomLayout data={questions} open="/sign-out" title="탈퇴관리">
@@ -75,8 +61,8 @@ const SignOutManage = () => {
       <Table
         pagination={{
           current: page,
-          pageSize: 10,
-          total: 20,
+          pageSize: questions?.totalElements,
+          total: questions?.totalPages,
           onChange: (page) => {
             setPage(page);
             navigate({
@@ -90,7 +76,7 @@ const SignOutManage = () => {
           return item.id;
         }}
         columns={columns}
-        dataSource={questions}
+        dataSource={questions?.content}
         loading={loading}
       />
     </CustomLayout>
