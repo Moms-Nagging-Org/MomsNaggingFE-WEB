d9af212e95a8ba65055d837807bc3b1c47152784
tooooo1 <wer4272@nate.com>
2022-09-03 23:58:17 +0900

    remove: mocks 주석으로 제거

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 
-import { worker } from './mocks/browser';
+// import { worker } from './mocks/browser';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start();
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
